b8c2b14eaef345a02d60cd5e6ce60563d820830e
Takumi Sugimoto <ts20030211@gmail.com>
2024-04-21 17:40:15 +0900

    update

--- src/bin/generate.rs
+++ src/bin/generate.rs
@@ -1,45 +1,45 @@
 use rand::prelude::*;
 
-const COUNT: usize = 100;
+const COUNT: usize = 1000;
 
-fn main() {
-    let mut writter = csv::WriterBuilder::new()
-        .delimiter(b',')
-        .from_path("generated.csv")
-        .unwrap();
-
-    let mut rng = thread_rng();
-    let lefts = choose_multiple("left.csv", COUNT, &mut rng);
-    let rights = choose_multiple("right.csv", COUNT, &mut rng);
-
-    for (i, (left, right)) in Iterator::zip(lefts.into_iter(), rights.into_iter()).enumerate() {
-        let index = format!("{}", i + 1);
-        let word = format!("{}{}", left, right);
-        writter.write_record(&[index, word]).unwrap();
-    }
+#[derive(Debug, serde::Deserialize)]
+struct InputRecord {
+    term: String,
+    local_freq: i64,
 }
 
-fn choose_multiple<R: Rng + ?Sized>(file_name: &str, amount: usize, rng: &mut R) -> Vec<String> {
-    let mut reader = csv::ReaderBuilder::default()
-        .delimiter(b',')
-        .from_path(file_name)
-        .unwrap();
+#[derive(Debug, serde::Serialize)]
+struct OutputRecord {
+    city: String,
+}
 
-    let mut words = vec![];
-    let mut weights = std::collections::HashMap::new();
-    for record in reader.records() {
-        let record = record.unwrap();
+fn main() {
+    let mut fst_reader = csv::Reader::from_path("fst_term.csv").unwrap();
+    let mut snd_reader = csv::Reader::from_path("snd_term.csv").unwrap();
 
-        let word = record.get(0).unwrap().parse::<String>().unwrap();
-        let weight = record.get(1).unwrap().parse::<f64>().unwrap();
-
-        words.push(word.clone());
-        weights.insert(word, weight);
+    let mut fst_col = std::collections::HashMap::<String, i64>::new();
+    for record in fst_reader.deserialize::<InputRecord>().flatten() {
+        fst_col.insert(record.term, record.local_freq);
     }
 
-    words
-        .choose_multiple_weighted(rng, amount, |x| *weights.get(x).unwrap())
-        .unwrap()
-        .cloned()
-        .collect::<Vec<_>>()
+    let mut snd_col = std::collections::HashMap::<String, i64>::new();
+    for record in snd_reader.deserialize::<InputRecord>().flatten() {
+        snd_col.insert(record.term, record.local_freq);
+    }
+
+    let mut rng = thread_rng();
+    let fst_sample = fst_col.keys().collect::<Vec<_>>();
+    let fst_sample = fst_sample
+        .choose_multiple_weighted(&mut rng, COUNT, |x| fst_col[*x] as f64)
+        .unwrap();
+    let snd_sample = snd_col.keys().collect::<Vec<_>>();
+    let snd_sample = snd_sample
+        .choose_multiple_weighted(&mut rng, COUNT, |x| snd_col[*x] as f64)
+        .unwrap();
+
+    let mut writer = csv::Writer::from_path("generated.csv").unwrap();
+    for (fst, snd) in Iterator::zip(fst_sample, snd_sample) {
+        let city = format!("{}{}", fst, snd);
+        writer.serialize(OutputRecord { city }).unwrap();
+    }
 }
--- src/bin/prepare.rs
+++ src/bin/prepare.rs
@@ -1,67 +1,97 @@
 use symspell::*;
 
+#[derive(Debug, serde::Deserialize)]
+struct InputRecord {
+    #[serde(rename = "City")]
+    city: String,
+}
+
+#[derive(Debug, serde::Serialize)]
+struct OutputRecord {
+    term: String,
+    local_freq: i64,
+    global_freq: i64,
+}
+
 fn main() {
     let mut spell: SymSpell<AsciiStringStrategy> = SymSpell::default();
     spell.load_dictionary("frequency_dictionary_en_82_765.txt", 0, 1, " ");
     spell.load_bigram_dictionary("frequency_bigramdictionary_en_243_342.txt", 0, 2, " ");
 
-    let mut reader = csv::ReaderBuilder::default()
-        .has_headers(true)
+    let mut reader = csv::ReaderBuilder::new()
         .delimiter(b'|')
         .from_path("city.csv")
         .unwrap();
 
-    let mut cities = std::collections::HashSet::new();
-    for record in reader.records() {
-        let record = record.unwrap();
-        let raw_city = record.get(0).unwrap();
-        let city = raw_city.replace(" ", "").to_lowercase();
-        cities.insert(city);
+    let mut text_col = std::collections::HashSet::new();
+    for record in reader.deserialize::<InputRecord>().flatten() {
+        let text = record.city;
+        let text = text.replace(" ", "").to_lowercase();
+        text_col.insert(text);
     }
 
-    let mut rights = vec![];
-    let mut lefts = vec![];
-    for city in cities {
-        let mut max_word_freq = 0;
-        let mut bi_word = None;
+    let mut global_freq = std::collections::HashMap::<String, i64>::new();
+    let mut fst_col = std::collections::HashMap::<String, i64>::new();
+    let mut snd_col = std::collections::HashMap::<String, i64>::new();
+    for text in text_col {
+        let mut fst_freq = 0;
+        let mut snd_freq = 0;
 
-        for i in 1..(city.len() - 1) {
-            let (left, right) = city.split_at(i);
-            let left_sug = spell.lookup(left, Verbosity::Top, 0);
-            let right_sug = spell.lookup(right, Verbosity::Top, 0);
+        let mut fst_term = String::new();
+        let mut snd_term = String::new();
 
-            if !left_sug.is_empty() && !right_sug.is_empty() {
-                let word_freq = i64::min(left_sug[0].count, right_sug[0].count);
+        for i in 1..text.len() {
+            let (fst, snd) = text.split_at(i);
+            let mut fst_sug = spell.lookup(fst, Verbosity::Top, 0);
+            let mut snd_sug = spell.lookup(snd, Verbosity::Top, 0);
 
-                if word_freq > max_word_freq {
-                    bi_word = Some((left_sug[0].term.clone(), right_sug[0].term.clone()));
-                    max_word_freq = word_freq;
-                }
+            if fst_sug.is_empty() || snd_sug.is_empty() {
+                continue;
+            }
+
+            let fst = fst_sug.swap_remove(0);
+            let snd = snd_sug.swap_remove(0);
+
+            if i64::min(fst.count, snd.count) > i64::min(fst_freq, snd_freq) {
+                fst_freq = fst.count;
+                snd_freq = snd.count;
+
+                fst_term = fst.term;
+                snd_term = snd.term;
             }
         }
 
-        if let Some((left, right)) = bi_word {
-            lefts.push(left);
-            rights.push(right);
+        if fst_term.is_empty() || snd_term.is_empty() {
+            continue;
         }
+
+        global_freq.insert(fst_term.clone(), fst_freq);
+        global_freq.insert(snd_term.clone(), snd_freq);
+        *fst_col.entry(fst_term).or_default() += 1;
+        *snd_col.entry(snd_term).or_default() += 1;
     }
 
-    write_words_to_csv(&lefts, "left.csv");
-    write_words_to_csv(&rights, "right.csv");
-}
-
-fn write_words_to_csv(words: &[String], file_name: &str) {
-    let mut writter = csv::WriterBuilder::new().from_path(file_name).unwrap();
-
-    let mut counter = std::collections::HashMap::<String, usize>::new();
-    for word in words {
-        *counter.entry(word.clone()).or_default() += 1;
+    let mut writter = csv::Writer::from_path("fst_term.csv").unwrap();
+    for (term, local_freq) in fst_col {
+        let global_freq = *global_freq.get(&term).unwrap();
+        writter
+            .serialize(OutputRecord {
+                term,
+                local_freq,
+                global_freq,
+            })
+            .unwrap();
     }
 
-    let mut words = counter.into_iter().collect::<Vec<_>>();
-    words.sort_by_key(|&(_, count)| count);
-
-    for (word, count) in words.iter().rev() {
-        writter.write_record(&[word, &count.to_string()]).unwrap();
+    let mut writter = csv::Writer::from_path("snd_term.csv").unwrap();
+    for (term, local_freq) in snd_col {
+        let global_freq = *global_freq.get(&term).unwrap();
+        writter
+            .serialize(OutputRecord {
+                term,
+                local_freq,
+                global_freq,
+            })
+            .unwrap();
     }
 }
