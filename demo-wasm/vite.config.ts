84c6671e4e9c28dd2f23498021d7495ae93e6331
Takumi Sugimoto <ts20030211@gmail.com>
2024-04-25 20:23:26 +0900

    fix github workflow

--- demo-wasm/vite.config.ts
+++ demo-wasm/vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
+  base: process.env.BASE_PATH,
   plugins: [solidPlugin()],
   server: {
     fs: {
       allow: [".."],
-    }
-  }
+    },
+  },
 });
