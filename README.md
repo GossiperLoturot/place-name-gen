45e5e96dc74970d01e0192ae34270eb38a231f27
Takumi Sugimoto <ts20030211@gmail.com>
2024-04-27 14:16:31 +0900

    update pipeline.sh and README.md

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
 # place-name-gen
 
+[![Rust](https://github.com/GossiperLoturot/place-name-gen/actions/workflows/rust.yml/badge.svg)](https://github.com/GossiperLoturot/place-name-gen/actions/workflows/rust.yml)
+
+Open [demo](https://gossiperloturot.github.io/place-name-gen/) for try it!
+
+## Description
+
+This provides a standalone place name generator that runs completely locally.
+It collects and randomly samples words before and after US and UK place names that can be divided into two words.
+Also, it can localize place names to Japanese.
+
+## Example
+
+```
+- downgate
+  ダウンゲイトゥ
+- burnburg
+  バーンバーグ
+- webstertree
+  ウェブスタートゥリー
+- clayown
+  クレイオウン
+- lowerdean
+  ロウアーディーン
+```
+
+## References
+
 Place database
 - https://github.com/grammakov/USA-cities-and-states
 - https://github.com/bwghughes/badbatch
--- pipeline.sh
+++ pipeline.sh
@@ -1,11 +1,62 @@
 #!/bin/sh
 
-cd $(dirname $0)
+FILENAME=$(basename $0)
 
-$(cd warmup && cargo run --release)
+# move to the directory of the script
+PRJ_DIR=$(cd $(dirname $0) && pwd)
+echo "[$FILENAME] Running pipeline in $PRJ_DIR"
 
-$(cd gen-native && cargo build --release)
+# check if the required tools are installed
+if type cargo &> /dev/null; then
+    echo "[$FILENAME] cargo is installed"
+else
+    echo "[$FILENAME] cargo is not installed"
+    exit 1
+fi
 
-$(cd gen-wasm && RUSTFLAGS= RUSTC_WRAPPER= wasm-pack build --release --target web)
+if type wasm-pack &> /dev/null; then
+    echo "[$FILENAME] wasm-pack is installed"
+else
+    echo "[$FILENAME] wasm-pack is not installed"
+    exit 1
+fi
 
-$(cd demo-wasm && bun i && bun run build && bun run serve)
+if type bun &> /dev/null; then
+    echo "[$FILENAME] bun is installed"
+else
+    echo "[$FILENAME] bun is not installed"
+    exit 1
+fi
+
+# run the pipeline
+echo "[$FILENAME] Warm up database"
+START_TIME=$(date +%s)
+cd $PRJ_DIR/warmup
+cargo run --release
+END_TIME=$(date +%s)
+echo "[$FILENAME] Done warmimp up database ($(($END_TIME - $START_TIME)) secs)"
+
+echo "[$FILENAME] Generate native lib"
+START_TIME=$(date +%s)
+cd $PRJ_DIR/gen-native
+cargo build --release
+END_TIME=$(date +%s)
+echo "[$FILENAME] Done generating native lib ($(($END_TIME - $START_TIME)) secs)"
+
+echo "[$FILENAME] Generate wasm lib"
+START_TIME=$(date +%s)
+cd $PRJ_DIR/gen-wasm
+RUSTFLAGS=
+RUSTC_WRAPPER=
+wasm-pack build --release --target web
+END_TIME=$(date +%s)
+echo "[$FILENAME] Done generating wasm lib ($(($END_TIME - $START_TIME)) secs)"
+
+echo "[$FILENAME] Build and run demo-wasm"
+START_TIME=$(date +%s)
+cd $PRJ_DIR/demo-wasm
+bun install
+bun run build
+bun run serve
+END_TIME=$(date +%s)
+echo "[$FILENAME] Done building and running $(($END_TIME - $START_TIME)) seconds"
